dce12f4294360f107e7f3502edc0e552adc4133c
theHasanas <30086691+theHasanas@users.noreply.github.com>
2020-12-27 02:20:27 +0300

    Remove solution code

--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,15 +1,7 @@
-import { GoToGithub, Wrapper, Emoji, Name } from "./styles";
+const InstructorTag = () => {
+  // Create the InstructorTag component here
 
-const InstructorTag = ({ emoji, name, github }) => {
-  const goToGitHubPage = () => window.open(`https://github.com/${github}`);
-
-  return (
-    <Wrapper onClick={goToGitHubPage}>
-      <Emoji>{emoji}</Emoji>
-      <Name>{name}</Name>
-      <GoToGithub>Go to GitHub</GoToGithub>
-    </Wrapper>
-  );
+  return <></>;
 };
 
 export default InstructorTag;
